f8c7a1806a61b49dbd60ecc8a8cdf8df2e6b3446
SARR <SARR@mbpdefatounew.home>
2026-02-13 23:21:14 +0100

    Add Google Analytics tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,28 @@
 // src/pages/_app.tsx
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-PW782X4LCD"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-PW782X4LCD');
+        `}
+      </Script>
+      
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
